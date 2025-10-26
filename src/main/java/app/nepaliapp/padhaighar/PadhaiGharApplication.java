647093f0f5a11c4fe5ac335ca8d8e6b964606d01
Asus <bsubash723@gmail.com>
2025-10-26 18:45:06 +0545

    Initial project commit

---
+++ src/main/java/app/nepaliapp/padhaighar/PadhaiGharApplication.java
@@ -0,0 +1,13 @@
+package app.nepaliapp.padhaighar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadhaiGharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadhaiGharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/nepaliapp/padhaighar/PadhaiGharApplicationTests.java
@@ -0,0 +1,13 @@
+package app.nepaliapp.padhaighar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadhaiGharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
